eecedc084effc6f394a66162118488bcf8320b12
Harry Hanskat <harryhanskat13@gmail.com>
2024-01-04 17:40:34 -0600

    Day 11 getting back in the swing of things

--- 2023/day11.py
+++ 2023/day11.py
@@ -5,14 +5,43 @@ Day 11 challenge
 """
 
 
-with open('2023/day11_input.txt', 'r') as f:
+with open('2023/day11_sample_input.txt', 'r') as f:
     data = f.read().splitlines()
 
+# Create variable that will hold data after expansion
+expanded_data = []
+
+# Create variables for x and y.
+# They begin as the size of the overall matrix.
+# We will then use 1 to denote if that line has a # in it and 0 if it doesn't
 x = list(range(len(data))) 
 y = list(range(len(data[0])))
 space = [x, y]
 
+# Set all values to 0 for both x and y
+for i, item in enumerate(x):
+    x[i] = 0
 
+for i, item in enumerate(y):
+    y[i] = 0
 
+# Iterate through lines (x axis) and if there's a '#' then the x axis and y axis get's flagged with a 1
 for x_index, line in enumerate(data):
-    continue
+    for y_index, character in enumerate(line):
+        if character == '#':
+            x[x_index] = 1
+            y[y_index] = 1
+
+# Now we have the correct values for the x and y lines that need to be duplicated 
+for i, y_blank in enumerate(y):
+    temp_line_to_insert = data[i] 
+    if y_blank == 0:
+        expanded_data.append(temp_line_to_insert)
+        expanded_data.append(temp_line_to_insert)
+    expanded_data.append(temp_line_to_insert)
+    
+print(expanded_data)
+
+for y_blank in y:
+
+    pass
