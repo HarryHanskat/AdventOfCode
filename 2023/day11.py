b90a41092f2402ac8f2ea88204b9f256e1d4d22b
Harry Hanskat <harryhanskat13@gmail.com>
2024-01-13 11:44:27 -0600

    Day 11 expanded data working

--- 2023/day11.py
+++ 2023/day11.py
@@ -26,22 +26,38 @@ for i, item in enumerate(y):
     y[i] = 0
 
 # Iterate through lines (x axis) and if there's a '#' then the x axis and y axis get's flagged with a 1
-for x_index, line in enumerate(data):
-    for y_index, character in enumerate(line):
+for y_index, line in enumerate(data):
+    for x_index, character in enumerate(line):
         if character == '#':
             x[x_index] = 1
             y[y_index] = 1
 
-# Now we have the correct values for the x and y lines that need to be duplicated 
+# Now we have the correct values for the x and y lines that need to be duplicated
+# Iterate through the data by each line
+#   If the corresponding y value == 1, add a variable that will denote inserting a duplicate line at the end of the loop.
+# Iterate through each value on the line
+#   if the x value == 1, then insert a '.' next to it.
+#   Do this until you complete the line 
 for i, y_blank in enumerate(y):
-    temp_line_to_insert = data[i] 
-    if y_blank == 0:
-        expanded_data.append(temp_line_to_insert)
-        expanded_data.append(temp_line_to_insert)
-    expanded_data.append(temp_line_to_insert)
     
-print(expanded_data)
+    temp_line = list(data[i])
+    additional_x_index = 0
+    
+    for x_index, x_blank in enumerate(x):
+        if x_blank == 0:
+           temp_line.insert(x_index + additional_x_index,'.')
+           additional_x_index += 1
+    
+    expanded_data.append(temp_line)
+    
+    if y_blank == 0:
+        expanded_data.append(temp_line)
 
-for y_blank in y:
+with open('2023/day11_expandedData.txt', 'w') as n_f:
+    newData = n_f
+    for line in expanded_data:
+        newData.write(''.join(line)+'\n') 
 
-    pass
+
+    
+print(newData)
